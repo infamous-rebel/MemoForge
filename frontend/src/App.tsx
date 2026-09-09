7b7a551431f05c3828879d199c3e59b47152753e
infamous-rebel <infamousrebelv@gmail.com>
2026-09-09 19:12:06 +0600

    Remove duplicate route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,6 +29,7 @@ export default function App() {
     <ToastProvider>
       <Routes>
         {/* Public */}
+        <Route path="/" element={<Landing />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/login" element={<Login />} />
 
